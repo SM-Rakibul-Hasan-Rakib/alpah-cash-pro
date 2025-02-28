74edd9386af37d0a566316ca72c3911be25a9cc3
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-02-28 22:45:32 +0600

    new funcation generate

--- script/alpah-clash.js
+++ script/alpah-clash.js
@@ -1,11 +1,21 @@
+// function play() {
+//   // step-1: hide the home screen. to hide the screen and the class hidden to the home section.
+
+//   const homeSection = document.getElementById("home-screen");
+//   homeSection.classList.add("hidden");
+
+//   // show the playground
+//   const playgroundSection = document.getElementById("play-ground");
+//   // console.log(playgroundSection.classList);
+//   playgroundSection.classList.remove("hidden");
+// }
+
+function contiuneGame() {
+  // step-1: generate a random alphbaet
+}
+
 function play() {
-  // step-1: hide the home screen. to hide the screen and the class hidden to the home section.
-
-  const homeSection = document.getElementById("home-screen");
-  homeSection.classList.add("hidden");
-
-  // show the playground
-  const playgroundSection = document.getElementById("play-ground");
-  // console.log(playgroundSection.classList);
-  playgroundSection.classList.remove("hidden");
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  contiuneGame();
 }
---
+++ script/main.js
@@ -0,0 +1,13 @@
+function hideElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
+}
+
+function showElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
+}
+
+function getRandomAlphbate() {
+  const alphbaetString = "abcdefghikjlmnopqrstuvwxyz";
+}
