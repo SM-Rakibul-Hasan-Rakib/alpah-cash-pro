565690f666d4ee8527341b096631ea219096f5cb
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-02-28 19:13:02 +0600

    add and remove

--- script/alpah-clash.js
+++ script/alpah-clash.js
@@ -1,3 +1,11 @@
 function play() {
-  console.log("game play");
+  // step-1: hide the home screen. to hide the screen and the class hidden to the home section.
+
+  const homeSection = document.getElementById("home-screen");
+  homeSection.classList.add("hidden");
+
+  // show the playground
+  const playgroundSection = document.getElementById("play-ground");
+  // console.log(playgroundSection.classList);
+  playgroundSection.classList.remove("hidden");
 }
